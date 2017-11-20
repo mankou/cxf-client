839c1a13629562953b8721fe572f26824ed736e9
mang <ningma003@gmail.com>
2017-11-20 15:13:24 +0800

    * 修改注释

--- src/main/java/mang/demo/cxf/client/Client4.java
+++ src/main/java/mang/demo/cxf/client/Client4.java
@@ -8,7 +8,8 @@ public class Client4 {
 	/**
 	 * 本例 演示 用JaxWsDynamicClientFactory 调用webService
 	 * 其不需要用工具生成客户端 只需要知道wsdl地址及调用的方法名  我感觉比较方便 但客户端里侵入了 cxf 的代码
-	 * 来自 http://blog.csdn.net/zy609398738/article/details/7484331 
+	 * 来自 http://blog.csdn.net/zy609398738/article/details/7484331
+	 * 官网参见 http://cxf.apache.org/docs/how-do-i-develop-a-client.html  关于Dynamic Client的章节
 	 * */
 	public static void main(String[] args) {
 //		String url="http://127.0.0.1:8080/cxf-spring/services/demoService?wsdl";
